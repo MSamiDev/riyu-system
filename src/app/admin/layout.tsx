f7562d80eaebe23673a903ce5b14b4687a88a38a
Mohammad Sami <mohammadsami@duck.com>
2024-06-07 05:06:31 +0530

    ADD: bg

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -473,7 +473,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 			{/* <!-- Content --> */}
 			<div className="w-full lg:ps-64">
-				<div className="p-4 sm:p-6 space-y-4 sm:space-y-6">
+				<div className="p-4 sm:p-6 space-y-4 sm:space-y-6 bg-red-100 min-h-screen">
 					{/* <!-- your content goes here ... --> */}
 					{children}
 				</div>
