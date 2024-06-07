368316635febb752d9430f7018a0144f5eee4ea6
Mohammad Sami <mohammadsami@duck.com>
2024-06-07 06:35:39 +0530

    ADD: Calendly schedule

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,9 +1,36 @@
+"use client"
+import Head from "next/head";
+import Script from 'next/script';
+
+import { useEffect } from "react";
+
 const page = () => {
-    return (
-        <div>
-            Calendar
-        </div>
-    );
-}
+	function handleClick() {
+        // @ts-ignore
+        window.Calendly.initPopupWidget({
+          url: 'https://calendly.com/mohammadsami',
+        });
+        return false;
+      }
+	return (
+		<div>
+			{/* <!-- Calendly inline widget begin --> */}
+			<>
+      <Script
+        src="https://assets.calendly.com/assets/external/widget.js"
+        type="text/javascript"
+        async
+      />
+      <link
+        href="https://assets.calendly.com/assets/external/widget.css"
+        rel="stylesheet"
+      />
+
+      <button onClick={handleClick}>Schedule</button>
+    </>
+			{/* <!-- Calendly inline widget end --> */}
+		</div>
+	);
+};
 
 export default page;
