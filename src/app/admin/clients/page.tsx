90b1b0841f7ce11b5f7693dcd3e17462d91d2baf
Mohammad Sami <mohammadsami@duck.com>
2024-06-07 12:16:22 +0530

    ADD: clients

--- src/app/admin/clients/page.tsx
+++ src/app/admin/clients/page.tsx
@@ -1,7 +1,76 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { db } from '@/config/firebase';
+import { collection, getDocs, DocumentData } from 'firebase/firestore';
+
+interface Client {
+    name: string;
+    email: string;
+    phone: string;
+    remainingMeetings: number;
+    subscriptionType: string;
+    subscriptionStart: string;
+    subscriptionEnd: string;
+    weight: number;
+    height: number
+  }
 const page = () => {
+    const [clients, setClients] = useState<Client[]>([]);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      const querySnapshot = await getDocs(collection(db, 'clients'));
+      const clientsData = querySnapshot.docs.map(doc => doc.data() as Client);
+      setClients(clientsData);
+    };
+    fetchClients();
+  }, []);
+
+
     return (
         <div>
-            Clients
+            <div className="flex flex-col">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <div className="border rounded-lg overflow-hidden dark:border-neutral-700">
+            <table className="min-w-full divide-y bg-white divide-gray-200 dark:divide-neutral-700">
+              <thead>
+                <tr>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Name</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Email</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Phone</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Height</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Weight</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Remaining Meetings</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Subscription Type</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Subscription Start</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Subscription End</th>
+                  <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Action</th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
+                {clients.map((client, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">{client.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.email}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.phone}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.height}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.weight}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.remainingMeetings}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.subscriptionType}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.subscriptionStart}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{client.subscriptionEnd}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                      <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400">Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
         </div>
     );
 }
