4aad118e193d73ac5f0de5229a54eb36ca060fea
Mohammad Sami <mohammadsami@duck.com>
2024-06-09 14:48:45 +0530

    ADD: firebase profile image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 export default MillionLint.next({
   rsc: true
 })(nextConfig);
